d6ec4a07dd560f7e8e912bd5d9748f95d28c8cdc
Ebenezer <ebenezer6374@gmail.com>
2026-09-07 21:26:04 +0530

    07/9 Learnings

---
+++ Leetcode/MergeTwoLists.java
@@ -0,0 +1,50 @@
+public class MergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+
+    public static void main(String[] args) {
+
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        MergeTwoLists solution = new MergeTwoLists();
+
+        ListNode result = solution.mergeTwoLists(list1, list2);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
